01a5b30d7e390027ac603fb3e5ea755acb0e4289
sairampogula6 <130931371+sairampogula6@users.noreply.github.com>
2023-04-17 01:12:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form id="payment-form">
+    <label for="card-number">Card number:</label>
+    <input type="text" id="card-number" name="card-number">
+  
+    <label for="expiry-date">Expiry date:</label>
+    <input type="text" id="expiry-date" name="expiry-date">
+  
+    <label for="cvv">CVV:</label>
+    <input type="text" id="cvv" name="cvv">
+  
+    <button type="submit">Pay</button>
+  </form>
+  <script src="script.js"></script>
+</body>
+</html>
